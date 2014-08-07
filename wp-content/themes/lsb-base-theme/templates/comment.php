adb3ee378b2d80f1a0a71ac215abf4b97c5b38c7
Benedicte Raae <raae@bgraphic.no>
2014-08-07 12:07:16 +0200

    Styling for roots comments

--- wp-content/themes/lsb-base-theme/templates/comment.php
+++ wp-content/themes/lsb-base-theme/templates/comment.php
@@ -1,7 +1,7 @@
 <?php echo get_avatar($comment, $size = '64'); ?>
 <div class="media-body">
   <h4 class="media-heading"><?php echo get_comment_author_link(); ?></h4>
-  <time datetime="<?php echo get_comment_date('c'); ?>"><a href="<?php echo htmlspecialchars(get_comment_link($comment->comment_ID)); ?>"><?php printf(__('%1$s', 'roots'), get_comment_date(),  get_comment_time()); ?></a></time>
+  <p><time datetime="<?php echo get_comment_date('c'); ?>"><a href="<?php echo htmlspecialchars(get_comment_link($comment->comment_ID)); ?>"><?php printf(__('%1$s', 'roots'), get_comment_date(),  get_comment_time()); ?></a></time></p>
   <?php edit_comment_link(__('(Edit)', 'roots'), '', ''); ?>
 
   <?php if ($comment->comment_approved == '0') : ?>
