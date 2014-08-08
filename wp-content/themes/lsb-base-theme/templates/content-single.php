4eaef6a5ac2fe44ad2a17ec16011686102e3b3cb
Benedicte Raae <raae@bgraphic.no>
2014-08-08 10:59:35 +0200

    Fixed base layout meta entry logic

--- wp-content/themes/lsb-base-theme/templates/content-single.php
+++ wp-content/themes/lsb-base-theme/templates/content-single.php
@@ -1,13 +1,15 @@
 <article <?php post_class(); ?>>
   <header>
     <?php get_template_part('templates/page', 'header'); ?>
-    <?php get_template_part('templates/entry-meta'); ?>
   </header>
   <div class="entry-content">
     <?php the_content(); ?>
   </div>
   <footer>
     <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
+    <?php if(is_single()) : ?>
+      <?php get_template_part('templates/entry-meta'); ?>
+    <?php endif; ?>
   </footer>
   <?php comments_template('/templates/comments.php'); ?>
 </article>
