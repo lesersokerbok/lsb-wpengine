5811d6bc2fe9cb849a8333c118b296886139eef5
Benedicte Raae <raae@bgraphic.no>
2014-08-08 10:06:28 +0200

    Better grid structure in footer

--- wp-content/themes/lsb-base-theme/templates/footer.php
+++ wp-content/themes/lsb-base-theme/templates/footer.php
@@ -1,17 +1,25 @@
 <footer class="widget-area">
   <div class="wrap container">
     <div class="row">
-      <div class="col-sm-3">
-        <?php dynamic_sidebar('sidebar-footer-1'); ?>
+      <div class="col-sm-6">
+        <div class="row">
+          <div class="col-md-6">
+            <?php dynamic_sidebar('sidebar-footer-1'); ?>
+          </div>
+          <div class="col-md-6">
+            <?php dynamic_sidebar('sidebar-footer-2'); ?>
+          </div>
+        </div>
       </div>
-      <div class="col-sm-3">
-        <?php dynamic_sidebar('sidebar-footer-2'); ?>
-      </div>
-      <div class="col-sm-3">
-        <?php dynamic_sidebar('sidebar-footer-3'); ?>
-      </div>
-      <div class="col-sm-3">
-        <?php dynamic_sidebar('sidebar-footer-4'); ?>
+      <div class="col-sm-6">
+        <div class="row">
+          <div class="col-md-6">
+            <?php dynamic_sidebar('sidebar-footer-3'); ?>
+          </div>
+          <div class="col-md-6">
+            <?php dynamic_sidebar('sidebar-footer-4'); ?>
+          </div>
+        </div>
       </div>
     </div>
   </div>
