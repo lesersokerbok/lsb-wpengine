508824120ba2ce188efaf99dc51d6b5620491612
Benedicte Raae <raae@bgraphic.no>
2014-08-08 08:34:15 +0200

    Had forgotten to register fourth footer sidebar

--- wp-content/themes/lsb-base-theme/lib/init.php
+++ wp-content/themes/lsb-base-theme/lib/init.php
@@ -71,5 +71,14 @@ function roots_widgets_init() {
     'before_title'  => '<h3>',
     'after_title'   => '</h3>',
   ));
+
+  register_sidebar(array(
+    'name'          => __('Footer Four', 'roots'),
+    'id'            => 'sidebar-footer-4s',
+    'before_widget' => '<section class="widget %1$s %2$s">',
+    'after_widget'  => '</section>',
+    'before_title'  => '<h3>',
+    'after_title'   => '</h3>',
+  ));
 }
 add_action('widgets_init', 'roots_widgets_init');
