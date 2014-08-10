b7df9170c3f786c487c8ca2f50f433801982e979
Tom Erik Støwer <testower@gmail.com>
2014-08-10 09:55:10 +0200

    Frontpage lists prototype

--- wp-content/themes/lsb-boksok.no-theme/template-boksok-frontpage.php
+++ wp-content/themes/lsb-boksok.no-theme/template-boksok-frontpage.php
@@ -4,64 +4,107 @@ Template Name: Boksøk Frontpage Template
 */
 ?>
 
-<?php while (have_posts()) : the_post(); ?>
+<section class="book-search">
+  <?php get_search_form(); ?>
+</section>
 
-  <?php if( have_rows('frontpage_list') ): ?>
+<?php if( have_rows('frontpage_list') ): ?>
 
-        <?php while ( have_rows('frontpage_list') ) : the_row(); ?>
+      <?php while ( have_rows('frontpage_list') ) : the_row(); ?>
 
-            <?php
+          <?php
+          $taxQuery = null;
 
-            global $post;
-
-            $taxQuery = array();
-
-            if (has_sub_field('customization')) :
-                _log('has_sub_field customization');
-                array_push($taxQuery, array(
-                    'taxonomy' => 'lsb_tax_customization',
-                    'field' => 'id',
-                    'terms' => get_sub_field('customization')
-                ));
-            endif;
-
-            if (has_sub_field('author')) :
-                _log('has_sub_field author');
-                array_push($taxQuery, array(
-                    'taxonomy' => 'lsb_tax_author',
-                    'field' => 'id',
-                    'terms' => get_sub_field('author')
-                ));
-            endif;
-
-            if (has_sub_field('genre')) :
-                _log('has_sub_field genre');
-                array_push($taxQuery, array(
-                    'taxonomy' => 'lsb_tax_genre',
-                    'field' => 'id',
-                    'terms' => get_sub_field('genre')
-                ));
-            endif;
-
-            $args = array(
-                'post_type' => 'lsb_book'
+          $age = null;
+          $age = get_sub_field('age');
+          if ($age) {
+            $taxQuery[] = array(
+              'taxonomy' => 'lsb_tax_age',
+              'field' => 'id',
+              'terms' => $age
             );
+          }
 
-            $custom_posts = get_posts($args);
+          $customization = null;
+          $customization = get_sub_field('customization');
+          if ($customization) {
+            $taxQuery[] = array(
+              'taxonomy' => 'lsb_tax_customization',
+              'field' => 'id',
+              'terms' => $customization
+            );
+          }
 
-            ?>
+          $author = null;
+          $author = get_sub_field('author');
+          if ($author) {
+            $taxQuery[] = array(
+              'taxonomy' => 'lsb_tax_author',
+              'field' => 'id',
+              'terms' => $author
+            );
+          }
 
-            <h2><?php the_sub_field('list-header'); ?></h2>
+          $genre = null;
+          $genre = get_sub_field('genre');
+          if ($genre) {
+            $taxQuery[] = array(
+              'taxonomy' => 'lsb_tax_genre',
+              'field' => 'id',
+              'terms' => $genre
+            );
+          }
 
-            <section class="loop">
-            <?php foreach($custom_posts as $post) : ?>
+          $topic = null;
+          $topic = get_sub_field('topic');
+          if ($topic) {
+            $taxQuery[] = array(
+              'taxonomy' => 'lsb_tax_topic',
+              'field' => 'id',
+              'terms' => $topic
+            );
+          }
+
+          $language = null;
+          $language = get_sub_field('language');
+          if ($language) {
+            $taxQuery[] = array(
+              'taxonomy' => 'lsb_tax_language',
+              'field' => 'id',
+              'terms' => $language
+            );
+          }
+
+          $publisher = null;
+          $publisher = get_sub_field('publisher');
+          if ($publisher) {
+            $taxQuery[] = array(
+              'taxonomy' => 'lsb_tax_publisher',
+              'field' => 'id',
+              'terms' => $publisher
+            );
+          }
+
+          $args = array(
+              'post_type' => 'lsb_book',
+              'tax_query' => $taxQuery
+          );
+
+          $wp_query = new WP_Query( $args );
+
+          ?>
+
+          <?php if ( $wp_query->have_posts() ) : ?>
+            <div class="row book-list">
+              <h2><?php the_sub_field('list-header'); ?></h2>
+              <?php while ( $wp_query->have_posts() ) : $wp_query->the_post(); ?>
                 <?php get_template_part('templates/content-summary', 'lsb_book'); ?>
-            <?php endforeach; ?>
-            </section>
+              <?php endwhile; ?>
+            </div>
+          <?php endif; ?>
 
-        <?php endwhile; ?>
+          <?php wp_reset_query(); ?>
 
-    <?php else: ?>
+      <?php endwhile; ?>
 
-    <?php endif; ?>
-<?php endwhile; ?>
+  <?php endif; ?>
