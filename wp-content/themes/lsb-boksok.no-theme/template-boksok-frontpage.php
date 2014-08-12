c96676f46d9465d6416a21e8068802892186c0d2
Tom Erik Støwer <testower@gmail.com>
2014-08-12 14:39:44 +0200

    Rename frontpage book lists to sections

--- wp-content/themes/lsb-boksok.no-theme/template-boksok-frontpage.php
+++ wp-content/themes/lsb-boksok.no-theme/template-boksok-frontpage.php
@@ -8,15 +8,15 @@ Template Name: Boksøk Frontpage Template
   <?php get_search_form(); ?>
 </section>
 
-<?php if( have_rows('frontpage_list') ): ?>
+<?php if( have_rows('frontpage_section') ): ?>
 
-      <?php while ( have_rows('frontpage_list') ) : the_row(); ?>
+      <?php while ( have_rows('frontpage_section') ) : the_row(); ?>
 
           <?php
           $taxQuery = null;
 
           $age = null;
-          $age = get_sub_field('age');
+          $age = get_sub_field('section-age');
           if ($age) {
             $taxQuery[] = array(
               'taxonomy' => 'lsb_tax_age',
@@ -26,7 +26,7 @@ Template Name: Boksøk Frontpage Template
           }
 
           $customization = null;
-          $customization = get_sub_field('customization');
+          $customization = get_sub_field('section-customization');
           if ($customization) {
             $taxQuery[] = array(
               'taxonomy' => 'lsb_tax_customization',
@@ -36,7 +36,7 @@ Template Name: Boksøk Frontpage Template
           }
 
           $author = null;
-          $author = get_sub_field('author');
+          $author = get_sub_field('section-author');
           if ($author) {
             $taxQuery[] = array(
               'taxonomy' => 'lsb_tax_author',
@@ -46,7 +46,7 @@ Template Name: Boksøk Frontpage Template
           }
 
           $genre = null;
-          $genre = get_sub_field('genre');
+          $genre = get_sub_field('section-genre');
           if ($genre) {
             $taxQuery[] = array(
               'taxonomy' => 'lsb_tax_genre',
@@ -56,7 +56,7 @@ Template Name: Boksøk Frontpage Template
           }
 
           $topic = null;
-          $topic = get_sub_field('topic');
+          $topic = get_sub_field('section-topic');
           if ($topic) {
             $taxQuery[] = array(
               'taxonomy' => 'lsb_tax_topic',
@@ -66,7 +66,7 @@ Template Name: Boksøk Frontpage Template
           }
 
           $language = null;
-          $language = get_sub_field('language');
+          $language = get_sub_field('section-language');
           if ($language) {
             $taxQuery[] = array(
               'taxonomy' => 'lsb_tax_language',
@@ -76,7 +76,7 @@ Template Name: Boksøk Frontpage Template
           }
 
           $publisher = null;
-          $publisher = get_sub_field('publisher');
+          $publisher = get_sub_field('section-publisher');
           if ($publisher) {
             $taxQuery[] = array(
               'taxonomy' => 'lsb_tax_publisher',
@@ -91,7 +91,8 @@ Template Name: Boksøk Frontpage Template
           );
 
           $orderby = null;
-          $orderby = get_sub_field('orderby');
+          $orderby = get_sub_field('section-orderby');
+          $order = get_sub_field('section-order');
           if ($orderby) {
               switch($orderby) {
                 case 'random':
@@ -99,12 +100,12 @@ Template Name: Boksøk Frontpage Template
                   break;
                 case 'added':
                   $args['orderby'] = 'date';
-                  $args['order'] = get_sub_field('order');
+                  $args['order'] = $order;
                   break;
                 case 'published':
                   $args['meta_key'] = 'lsb_published_year';
                   $args['orderby'] = 'meta_value_num';
-                  $args['order'] = get_sub_field('order');
+                  $args['order'] = $order;
                   $args['meta_query'] = array(
                     array(
                       'key' => 'lsb_published_year'
@@ -123,15 +124,15 @@ Template Name: Boksøk Frontpage Template
           ?>
 
           <?php if ( $wp_query->have_posts() ) : ?>
-            <div class="book-list">
-              <div class="book-list-header page-header">
+            <div class="book-section">
+              <div class="book-section-header page-header">
 
                 <h1>
-                  <?php the_sub_field('list-header'); ?>
-                  <?php if ( get_sub_field('list-sub-header') ) : ?>
-                    <small>| <?php the_sub_field('list-sub-header'); ?></small>
+                  <?php the_sub_field('section-header'); ?>
+                  <?php if ( get_sub_field('section-sub-header') ) : ?>
+                    <small>| <?php the_sub_field('section-sub-header'); ?></small>
                   <?php endif; ?>
-                  <?php if ( get_sub_field('description') ) : ?>
+                  <?php if ( get_sub_field('section-description') ) : ?>
                     <small aria-hidden="true">
                       | <button type="button" class="btn-link">
                           <span class="glyphicon glyphicon-info-sign"></span>
@@ -141,24 +142,24 @@ Template Name: Boksøk Frontpage Template
                   <?php endif; ?>
                 </h1>
 
-                <?php if ( get_sub_field('description') ) : ?>
+                <?php if ( get_sub_field('section-description') ) : ?>
                   <p class="alert alert-info description sr-only">
                     <button type="button" class="close">
                       <span aria-hidden="true">&times;</span>
                       <span class="sr-only">Close</span>
                     </button>
-                    <?php the_sub_field('description'); ?>
+                    <?php the_sub_field('section-description'); ?>
                   </p>
                 <?php endif; ?>
 
               </div>
 
-              <div class="book-list-body">
+              <div class="book-section-body">
 
-                <span aria-hidden="true" class="book-list-left-scroll hidden-xs glyphicon glyphicon-chevron-left"></span>
-                <span aria-hidden="true" class="book-list-right-scroll hidden-xs glyphicon glyphicon-chevron-right"></span>
+                <span aria-hidden="true" class="book-section-left-scroll hidden-xs glyphicon glyphicon-chevron-left"></span>
+                <span aria-hidden="true" class="book-section-right-scroll hidden-xs glyphicon glyphicon-chevron-right"></span>
 
-                <div class="book-list-scroll">
+                <div class="book-section-scroll">
 
                   <?php while ( $wp_query->have_posts() ) : $wp_query->the_post(); ?>
                     <?php get_template_part('templates/content-summary', 'lsb_book'); ?>
