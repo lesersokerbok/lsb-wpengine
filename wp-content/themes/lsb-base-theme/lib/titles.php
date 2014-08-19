102df1d0060dfab7400d28ac0c6a062897c6e33d
Benedicte Raae <raae@bgraphic.no>
2014-08-19 10:32:03 +0200

    Better search result title

--- wp-content/themes/lsb-base-theme/lib/titles.php
+++ wp-content/themes/lsb-base-theme/lib/titles.php
@@ -27,7 +27,7 @@ function roots_title() {
     }
 
   } elseif (is_search()) {
-    return sprintf(__('Resultater for søk etter %s', 'lsb'), get_search_query());
+    return sprintf(__('Søkeresultat for: "%s"', 'lsb'), get_search_query());
   } elseif (is_404()) {
     return __('Fant ikke det du lette etter', 'lsb');
   } else {
