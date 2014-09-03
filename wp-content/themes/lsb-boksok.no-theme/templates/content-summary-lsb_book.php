cddc32c594c458cd9e6c62a9d5d88c5d2dfb51ee
Benedicte Raae <raae@bgraphic.no>
2014-09-03 11:13:46 +0200

    Cleaned up book summary template

--- wp-content/themes/lsb-boksok.no-theme/templates/content-summary-lsb_book.php
+++ wp-content/themes/lsb-boksok.no-theme/templates/content-summary-lsb_book.php
@@ -17,9 +17,8 @@
   <header>
     <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
     <p class="meta">
-      <?php the_terms($post->ID, 'lsb_tax_author') ?><br/>
-      <?php the_terms($post->ID, 'lsb_tax_publisher') ?><br/>
-      <?php the_field('year_published'); ?>
+      <?php the_terms($post->ID, 'lsb_tax_author', '', ', ', '<br/>') ?>
+      <?php the_terms($post->ID, 'lsb_tax_publisher', '', ', ', '<br/>') ?>
     </p>
   </header>
 </article>
