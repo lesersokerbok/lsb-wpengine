4d2f33cad4458d38ec7e63f8d03447e1c3ef8b58
Benedicte Raae <raae@bgraphic.no>
2014-12-18 13:21:50 +0100

    Never show sidebar for frontpage template #43

--- wp-content/themes/lsb-boksok.no-theme/lib/config.php
+++ wp-content/themes/lsb-boksok.no-theme/lib/config.php
@@ -77,7 +77,7 @@ function roots_display_sidebar() {
      * Any of these page templates that return true won't show the sidebar
      */
     array(
-      'template-custom.php'
+      'template-boksok-frontpage.php'
     )
   );
 
