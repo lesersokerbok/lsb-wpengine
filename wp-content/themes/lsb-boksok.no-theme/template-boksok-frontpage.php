70fb7b7901bff39b6f339130d8783b7ba5fb6ce8
Tom Erik Støwer <testower@gmail.com>
2014-12-18 09:49:16 +0100

    Revert "Moved search field to header"
    This reverts commit db67d8412ae55a2b3ff361c0b73c6f08a3f3eb80.

--- wp-content/themes/lsb-boksok.no-theme/template-boksok-frontpage.php
+++ wp-content/themes/lsb-boksok.no-theme/template-boksok-frontpage.php
@@ -4,6 +4,10 @@ Template Name: Boksøk Frontpage Template
 */
 ?>
 
+<section class="book-search">
+  <?php get_search_form(); ?>
+</section>
+
 <?php if ( have_rows('book_section') ): ?>
   <?php while ( have_rows('book_section') ) : the_row(); ?>
     <?php get_template_part('templates/book-section', get_sub_field('section_type')); ?>
