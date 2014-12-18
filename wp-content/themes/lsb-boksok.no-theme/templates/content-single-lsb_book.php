c80dd56ae8ba5f7d6c4a4cd5405c9985c884dd33
Benedicte Raae <raae@bgraphic.no>
2014-12-18 13:54:57 +0100

    Added taxonomies to single book pages

--- wp-content/themes/lsb-boksok.no-theme/templates/content-single-lsb_book.php
+++ wp-content/themes/lsb-boksok.no-theme/templates/content-single-lsb_book.php
@@ -80,11 +80,22 @@
 
         <div class="panel panel-default">
           <div class="panel-body">
+            <?php the_terms($post->ID, 'lsb_tax_genre', __('Hovedkategori: ', 'lsb_boksok'), ', ', '<br/>') ?>
+            <?php the_terms($post->ID, 'lsb_tax_age', __('Alder: ', 'lsb_boksok'), ', ', '<br/>') ?>
+            <?php the_terms($post->ID, 'lsb_tax_audience', __('Tilpasset: ', 'lsb_boksok'), ', ', '<br/>') ?>
             <?php the_terms($post->ID, 'lsb_tax_genre', __('Sjanger: ', 'lsb_boksok'), ', ', '<br/>') ?>
             <?php the_terms($post->ID, 'tax_lsb_language', __('Språk: ', 'lsb_boksok'), ', ', '<br/>') ?>
-            <?php the_terms($post->ID, 'lsb_tax_age', __('Passer for: ', 'lsb_boksok'), ', ', '<br/>') ?>
-            <?php the_terms($post->ID, 'lsb_tax_customization', __('Tilpasning: ', 'lsb_boksok'), ', ', '<br/>') ?>
             <?php the_terms($post->ID, 'lsb_tax_topic', __('Tema: ', 'lsb_boksok'), ', ', '<br/>') ?>
+            
+            <?php if(has_term('', 'lsb_tax_series') || has_term('', 'lsb_tax_list')): ?>
+              En del av: 
+              <?php if(has_term('', 'lsb_tax_series')): ?>
+                <?php the_terms($post->ID, 'lsb_tax_list', '', ', ', ',') ?>
+              <?php else: ?>
+                <?php the_terms($post->ID, 'lsb_tax_list', '', ', ', '<br/>') ?>
+              <?php endif; ?>
+              <?php the_terms($post->ID, 'lsb_tax_series', '', ', ', '<br/>') ?>
+            <?php endif; ?>
 
             <?php if( get_field( "lsb_pages" ) ): ?>
               <?php echo __('Antall sider: ', 'lsb_boksok'); ?>
