af283abc93b554e820c448b2e5ea15d42066be6a
Benedicte Raae <raae@bgraphic.no>
2014-12-18 10:54:02 +0100

    Added audience tax and changed norwegian name of lsb_cat to hovedkatoegori #40

--- wp-content/themes/lsb-boksok.no-theme/lib/lsb-book.php
+++ wp-content/themes/lsb-boksok.no-theme/lib/lsb-book.php
@@ -13,6 +13,7 @@ class LsbBook {
     add_action('init', array($this, 'register_tax_lsb_topic'));
     add_action('init', array($this, 'register_tax_lsb_language'));
     add_action('init', array($this, 'register_tax_lsb_cat'));
+    add_action('init', array($this, 'register_tax_lsb_audience'));
     add_action('init', array($this, 'register_lsb_tax_list'));
     add_action('init', array($this, 'register_lsb_tax_series'));
     add_action('init', array($this, 'register_lsb_acf_book_meta'));
@@ -323,21 +324,51 @@ class LsbBook {
       ),
       array(
         'hierarchical' => true,
-        'label' => __('LSB kategori', 'lsb_boksok'),
+        'label' => __('Hovedkategori', 'lsb_boksok'),
         'show_ui' => true,
         'query_var' => true,
-        'rewrite' => array( 'slug' => _x('lsb-kategori', 'lsb_tax_lsb_cat slug', 'lsb_boksok') ),
+        'rewrite' => array( 'slug' => _x('hovedkategori', 'lsb_tax_lsb_cat slug', 'lsb_boksok') ),
         'show_admin_column' => false,
         'labels' => array (
-          'search_items' => __('LSB kategori', 'lsb_boksok'),
+          'search_items' => __('Hovedkategori', 'lsb_boksok'),
           'popular_items' => __('Populære', 'lsb_boksok'),
           'all_items' => __('Alle', 'lsb_boksok'),
-          'parent_item' => __('LSB kategori', 'lsb_boksok'),
-          'parent_item_colon' => __('LSB kategori: ', 'lsb_boksok'),
-          'edit_item' => __('Rediger LSB kategori', 'lsb_boksok'),
-          'update_item' => __('Oppdater LSB kategori', 'lsb_boksok'),
-          'add_new_item' => __('Legg til LSB kategori', 'lsb_boksok'),
-          'new_item_name' => _x('LSB kategori', 'Ny-lsb-cat-tittel', 'lsb_boksok'),
+          'parent_item' => __('Hovedkategori', 'lsb_boksok'),
+          'parent_item_colon' => __('Hovedkategori: ', 'lsb_boksok'),
+          'edit_item' => __('Rediger Hovedkategori', 'lsb_boksok'),
+          'update_item' => __('Oppdater Hovedkategori', 'lsb_boksok'),
+          'add_new_item' => __('Legg til Hovedkategori', 'lsb_boksok'),
+          'new_item_name' => _x('Hovedkategori', 'Ny-lsb-cat-tittel', 'lsb_boksok'),
+          'separate_items_with_commas' => __('Separer med komma', 'lsb_boksok'),
+          'add_or_remove_items' => __('Legg til eller fjern', 'lsb_boksok'),
+          'choose_from_most_used' => __('Velg fra mest brukte', 'lsb_boksok'),
+        )
+      )
+    );
+  }
+  
+  public function register_tax_lsb_audience() {
+    register_taxonomy( 'lsb_tax_lsb_audience',
+      array(
+        0 => 'lsb_book'
+      ),
+      array(
+        'hierarchical' => true,
+        'label' => __('Målgruppe', 'lsb_boksok'),
+        'show_ui' => true,
+        'query_var' => true,
+        'rewrite' => array( 'slug' => _x('leser', 'lsb_tax_lsb_audience slug', 'lsb_boksok') ),
+        'show_admin_column' => false,
+        'labels' => array (
+          'search_items' => __('Målgruppe', 'lsb_boksok'),
+          'popular_items' => __('Populære', 'lsb_boksok'),
+          'all_items' => __('Alle', 'lsb_boksok'),
+          'parent_item' => __('Målgruppe', 'lsb_boksok'),
+          'parent_item_colon' => __('Målgruppe: ', 'lsb_boksok'),
+          'edit_item' => __('Rediger Målgruppe', 'lsb_boksok'),
+          'update_item' => __('Oppdater Målgruppe', 'lsb_boksok'),
+          'add_new_item' => __('Legg til Målgruppe', 'lsb_boksok'),
+          'new_item_name' => _x('Målgruppe', 'Ny-lsb-cat-tittel', 'lsb_boksok'),
           'separate_items_with_commas' => __('Separer med komma', 'lsb_boksok'),
           'add_or_remove_items' => __('Legg til eller fjern', 'lsb_boksok'),
           'choose_from_most_used' => __('Velg fra mest brukte', 'lsb_boksok'),
