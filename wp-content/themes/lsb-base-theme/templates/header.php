7cacec197c00d2aa4bcd56e367c2bbcc95ad3c02
Benedicte Raae <raae@bgraphic.no>
2015-02-23 10:54:01 +0100

    Removed meny test

--- wp-content/themes/lsb-base-theme/templates/header.php
+++ wp-content/themes/lsb-base-theme/templates/header.php
@@ -8,9 +8,6 @@
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </div>
-        <div class="text">
-          <?php _e('Meny', 'lsb'); ?>
-        </div>
       </button>
 
       <a class="navbar-brand" href="<?php echo home_url(); ?>/">
