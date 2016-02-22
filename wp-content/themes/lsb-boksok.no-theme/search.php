8dd519415c5c600705d87512e75b17222ed6d043
Benedicte Raae <raae@bgraphic.no>
2016-02-22 16:41:57 +0100

    New page header on search as well.

--- wp-content/themes/lsb-boksok.no-theme/search.php
+++ wp-content/themes/lsb-boksok.no-theme/search.php
@@ -6,39 +6,33 @@
   $has_posts = have_posts() && !$searchwp_filter_no_results;
 ?>
 
-<?php get_template_part('templates/page', 'header'); ?>
-
-<section class="book-search">
-  <?php get_search_form(); ?>
-</section>
-
-<?php if(!$has_posts && $alert_text) : ?>
-  <p class="alert alert-warning">
-    <?php _e('Beklager, ingen søkeresultater', 'lsb'); ?> <?php echo $alert_text; ?>
-    Søk etter <i><?php echo get_search_query()?></i>
-    <a href="/?s=<?php echo get_search_query()?>">
-       i alle bøker.
-    </a>
-  </p>
-<?php elseif (!$has_posts) : ?>
-  <div class="alert alert-warning">
-    <?php _e('Beklager, ingen søkeresultater', 'lsb'); ?>.
+<div class="page-header">
+  <div>
+    <header>
+      <h1>
+        <?php echo roots_title(); ?>
+      </h1>
+      <?php if($alert_text) : ?>
+        <p>
+          <?php _e('Viser kun søkeresultater', 'lsb'); ?> <?php echo $alert_text; ?><br/>
+          Søk etter <i><?php echo get_search_query()?></i>
+          <a href="/?s=<?php echo get_search_query()?>">
+            <?php _e('i alle bøker.'); ?>
+          </a>
+        </p>
+      <?php endif; ?>
+    </header>
+    <?php get_search_form(); ?>
   </div>
-<?php elseif($alert_text) : ?>
-  <p class="alert alert-info">
-    <?php _e('Viser kun søkeresultater', 'lsb'); ?> <?php echo $alert_text; ?>
-    Søk etter <i><?php echo get_search_query()?></i>
-    <a href="/?s=<?php echo get_search_query()?>">
-       i alle bøker.
-    </a>
-  </p>
-<?php endif; ?>
+</div>
 
 <section class="loop">
 <?php if($has_posts) : ?>
   <?php while (have_posts()) : the_post(); ?>
     <?php get_template_part('templates/content-summary', get_post_type()); ?>
   <?php endwhile; ?>
+<?php else : ?>
+
 <?php endif; ?>
 </section>
 
