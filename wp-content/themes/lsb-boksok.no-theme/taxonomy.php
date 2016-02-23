3c2c2e71554c9d30b0a26573b0d24c7bc1d2a223
Benedicte Raae <raae@bgraphic.no>
2016-02-23 17:12:38 +0100

    After page sections the actual taxonomy archive is displayed with an "archive" heading.

--- wp-content/themes/lsb-boksok.no-theme/taxonomy.php
+++ wp-content/themes/lsb-boksok.no-theme/taxonomy.php
@@ -16,6 +16,13 @@
 
 <?php if ( have_posts() ): ?>
   <section class="loop">
+    <?php if( !is_paged() && have_rows('lsb_page_sections', get_queried_object()) ) : ?>
+    <div class="page-section-header">
+      <h1>
+        <?php esc_html_e( 'Arkiv', 'lsb' ); ?> <small> | <?php echo roots_title(); ?></small>
+      </h1>
+    </div>
+    <?php endif; ?>
     <?php while (have_posts()) : the_post(); ?>
       <?php get_template_part('templates/content-summary', get_post_type()); ?>
     <?php endwhile; ?>
