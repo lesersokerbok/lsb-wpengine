a21ee68e1b3115e23352b27e0f0cfc3f48af5cbf
Benedicte Raae <raae@bgraphic.no>
2016-03-11 17:36:38 +0100

    Added a filter option to the widgets

--- wp-content/plugins/lsb-boksok-search-widget/lsb-boksok-search-widget.php
+++ wp-content/plugins/lsb-boksok-search-widget/lsb-boksok-search-widget.php
@@ -48,6 +48,9 @@ class LSB_Boksok_Search_Widget extends WP_Widget {
 
 		<div class="textwidget"><?php echo wpautop( $instance['description'] ); ?></div>
 		<form action="http://boksok.no/" method="get">
+			<?php if( ! empty ( $instance['main_cat_filter'] )) : ?>
+				<input name="hovedkategori" value="<?php echo $instance['main_cat_filter']; ?>" type="hidden">
+			<? endif; ?>
       		<input name="s" placeholder="<?php echo !empty( $instance['placeholder'] ) ? $instance['placeholder'] : ''; ?>" type="search">
       		<input value="Søk" type="submit">
     	</form>
@@ -66,6 +69,7 @@ class LSB_Boksok_Search_Widget extends WP_Widget {
 		$title = isset( $instance['title'] ) ? $instance['title'] : __( 'Boksøk', 'lsb_boksok_search_widget' );
 		$description = isset( $instance['description'] ) ? $instance['description'] : __( 'Søk etter bøker på boksøk.no.', 'lsb_boksok_search_widget' );
 		$placeholder = isset( $instance['placeholder'] ) ? $instance['placeholder'] : __( 'Søk etter en forfatter, en tittel eller et tema!', 'lsb_boksok_search_widget' );
+		$main_cat_filter = isset( $instance['main_cat_filter'] ) ? $instance['main_cat_filter'] : 'no-filter';
 
 		?>
 		<p>
@@ -86,7 +90,16 @@ class LSB_Boksok_Search_Widget extends WP_Widget {
 			</label>
 			<input class="widefat" id="<?php echo $this->get_field_id( 'placeholder' ); ?>" name="<?php echo $this->get_field_name( 'placeholder' ); ?>" type="text" value="<?php echo esc_attr( $placeholder ); ?>">
 		</p>
-
+		<p>
+			<label for="<?php echo $this->get_field_id( 'main_cat_filter' ); ?>"><?php _e( 'Filtrer på kategori:', 'lsb_boksok_search_widget' ); ?></label><br/>
+			<select id="<?php echo $this->get_field_id( 'main_cat_filter' ); ?>" name="<?php echo $this->get_field_name( 'main_cat_filter' ); ?>">
+				<option value="no-filter" <?php echo ('no-filter' == $main_cat_filter) ? 'selected' : ''; ?>><?php _e( 'Inget filter', 'lsb_boksok_search_widget' ); ?></option>
+				<option value="litt-a-lese" <?php echo ('litt-a-lese' == $main_cat_filter) ? 'selected' : ''; ?>><?php _e( 'Litt å lese', 'lsb_boksok_search_widget' ); ?></option>
+				<option value="enkelt-innhold" <?php echo ('enkelt-innhold' == $main_cat_filter) ? 'selected' : ''; ?>><?php _e( 'Enkelt innhold', 'lsb_boksok_search_widget' ); ?></option>
+				<option value="punktskrift-folebilder" <?php echo ('punktskrift-folebilder' == $main_cat_filter) ? 'selected' : ''; ?>><?php _e( 'Punkskrift & følebilder', 'lsb_boksok_search_widget' ); ?></option>
+				<option value="tegnsprak-nmt" <?php echo ('tegnsprak-nmt' == $main_cat_filter) ? 'selected' : ''; ?>><?php _e( 'Tegnspråk & NMT', 'lsb_boksok_search_widget' ); ?></option>
+				<option value="bliss-piktogram" <?php echo ('bliss-piktogram' == $main_cat_filter) ? 'selected' : ''; ?>><?php _e( 'Bliss & piktogram', 'lsb_boksok_search_widget' ); ?></option>
+			</select>
 	<?php
 	}
 	/**
@@ -105,6 +118,7 @@ class LSB_Boksok_Search_Widget extends WP_Widget {
 		$instance['description'] = ( ! empty( $new_instance['description'] ) ) ? strip_tags( $new_instance['description'] ) : '';
 		$instance['filter'] = ! empty( $new_instance['filter'] );
 		$instance['placeholder'] = ( ! empty( $new_instance['placeholder'] ) ) ? strip_tags( $new_instance['placeholder'] ) : '';
+		$instance['main_cat_filter'] = ( ! empty( $new_instance['main_cat_filter'] ) ) ? strip_tags( $new_instance['main_cat_filter'] ) : '';
 		return $instance;
 	}
 } // class My_Widget
