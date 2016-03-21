61a3a4c13b95230d68c3ce89f9b15367c5a8cc14
Benedicte Raae <raae@bgraphic.no>
2016-03-21 14:48:16 +0100

    Small text changes

--- wp-content/themes/lsb-boksok.no-theme/templates/book-partials/meta1.php
+++ wp-content/themes/lsb-boksok.no-theme/templates/book-partials/meta1.php
@@ -4,8 +4,8 @@
       <?php the_terms($post->ID, 'lsb_tax_author', '<li>'.__('Forfatter: ', 'lsb_boksok'), ',', '</li>') ?>
       <?php the_terms($post->ID, 'lsb_tax_illustrator', '<li>'.__('Illustratør: ', 'lsb_boksok'), ', ', '</li>') ?>
       <?php TaxonomyUtil::the_unhidden_term_list($post->ID, 'lsb_tax_topic', '<li>'.__('Tema: ', 'lsb_boksok'), ', ', '</li>') ?>
-      <?php the_terms($post->ID, 'lsb_tax_audience', '<li>'.__('Tilpasset ', 'lsb_boksok'), ', ', '</li>') ?>
-      <?php the_terms($post->ID, 'lsb_tax_age', '<li>'.__('Boken passer for ', 'lsb_boksok'), ', ', '</li>') ?>
+      <?php the_terms($post->ID, 'lsb_tax_audience', '<li>'.__('Tilpasset: ', 'lsb_boksok'), ', ', '</li>') ?>
+      <?php the_terms($post->ID, 'lsb_tax_age', '<li>'.__('Alder: ', 'lsb_boksok'), ', ', '</li>') ?>
     </ul>
   </div>
 </div>
--- wp-content/themes/lsb-boksok.no-theme/templates/book-partials/more.php
+++ wp-content/themes/lsb-boksok.no-theme/templates/book-partials/more.php
@@ -1,7 +1,7 @@
 <?php if (has_term('', 'lsb_tax_series') || has_term('', 'lsb_tax_list')) : ?>
 <div class="more panel panel-default">
   <div class="panel-body">
-    <p><?php _e('Boken er en del av: ', 'lsb'); ?></p>
+    <p><?php _e('Boka er en del av: ', 'lsb'); ?></p>
     <ul>
       <?php the_terms($post->ID, 'lsb_tax_series', '<li>', '</li><li>', '</li>') ?>
       <?php the_terms($post->ID, 'lsb_tax_list', '<li>', '</li><li>', '</li>') ?>
