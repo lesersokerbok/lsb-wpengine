1f987b8af1a86534387123fae88e4e0dd88ef2a3
Benedicte Raae <raae@bgraphic.no>
2016-03-21 13:40:07 +0100

    Fixed lsb-supported display issues.

--- wp-content/themes/lsb-boksok.no-theme/templates/book-partials/lsb-supported.php
+++ wp-content/themes/lsb-boksok.no-theme/templates/book-partials/lsb-supported.php
@@ -1,9 +1,7 @@
 <?php if ( get_field('lsb_supported')): ?>
-  <div class="lsb-supported panel panel-default">
+  <div class="lsb-supported <?php the_field('lsb_support_cat') ?> panel panel-default">
     <div class="panel-body">
-      <p class="lsb-supported <?php the_field('lsb_support_cat') ?>">
-        <?php echo __('Boken er støttet av Leser søker bok', 'lsb_boksok'); ?>
-      </p>
+      <?php echo __('Boken er støttet av Leser søker bok', 'lsb_boksok'); ?>
     </div>
   </div>
 <?php endif; ?>
