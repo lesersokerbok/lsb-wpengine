e2c69f0cdfa3fc25104dc015c08f9f310ad5f4a6
Benedicte Raae <raae@bgraphic.no>
2016-03-21 13:12:25 +0100

    Check if book is part of list or series. Changed text.

--- wp-content/themes/lsb-boksok.no-theme/templates/book-partials/more.php
+++ wp-content/themes/lsb-boksok.no-theme/templates/book-partials/more.php
@@ -1,8 +1,11 @@
+<?php if (has_term('', 'lsb_tax_series') || has_term('', 'lsb_tax_list')) : ?>
 <div class="more panel panel-default">
   <div class="panel-body">
+    <p><?php _e('Boken er en del av: ', 'lsb'); ?></p>
     <ul>
-      <?php the_terms($post->ID, 'lsb_tax_series', '<li>'.__('Se flere bøker fra samme serie: '), ', ', '</li>') ?>
-      <?php the_terms($post->ID, 'lsb_tax_list', '<li>'.__('Se flere bøker fra samme liste: '), ', ', '</li>') ?>
+      <?php the_terms($post->ID, 'lsb_tax_series', '<li>', '</li><li>', '</li>') ?>
+      <?php the_terms($post->ID, 'lsb_tax_list', '<li>', '</li><li>', '</li>') ?>
     </ul>
   </div>
 </div>
+<?php endif ; ?>
