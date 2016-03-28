bc339ba1d88d7023c0d7790626f3f39ad14e7b1b
Benedicte Raae <raae@bgraphic.no>
2016-03-28 20:38:03 +0200

    Admin notice if post type lsb_book does not exist.

--- wp-content/plugins/lsb-bibsyst-integration/lsb-bibsyst-integration.php
+++ wp-content/plugins/lsb-bibsyst-integration/lsb-bibsyst-integration.php
@@ -11,7 +11,31 @@
 include('class-isbn-feed.php');
 include('class-status-importer.php');
 
+class LSB_Bibsyst_Integration {
 
+	function __construct() {
+		add_action( 'admin_init', array( $this, 'check_dependencies' ) );
+    }
+
+	function check_dependencies() {
+		if ( ! self::dependencies_fullfilled() ) {
+            add_action( 'admin_notices', array( $this, 'dependencies_notice' ) );
+        }
+	}
+
+    function dependencies_notice() {
+		echo '<div class="notice notice-warning">';
+		echo '<p>' . esc_html__( 'The LSB Bibsyst Integration plugin requires the post type "lsb_book".', 'lsb-bibsyst-integration' ) . '</p>';
+		echo '<p>' . esc_html__( 'Deactivate and activate again after "lsb_book" is in place.', 'lsb-bibsyst-integration' ) . '</p>';
+		echo '</div>';
+    }
+
+    static function dependencies_fullfilled() {
+        return post_type_exists( 'lsb_book' );
+    }
+}
+
+$bibsyst_integration = new LSB_Bibsyst_Integration();
 $isbn_feed = new LSB_Bibsyst_Isbn_Feed();
 $status_importer = new LSB_Bibsyst_Status_Importer();
 
