313058407945465eed7c74f3a4a644eb0ae04181
Benedicte Raae <raae@bgraphic.no>
2016-03-29 15:12:18 +0200

    Better library status styling

--- wp-content/themes/lsb-boksok.no-theme/templates/book-partials/library-status.php
+++ wp-content/themes/lsb-boksok.no-theme/templates/book-partials/library-status.php
@@ -6,19 +6,29 @@ $counties = get_post_meta($post->ID, 'lsb_library_status', true);
 
 <?php if( $counties ) : ?>
 <div class="content-part library-status">
+
   <?php foreach( $counties as $county_libraries ) : ?>
-    <?php echo esc_html( $county_libraries[0]['county'] ) ?>
-    <ul>
+  <table class="table">
+    <thead>
+      <tr><th colspan="2"><?php echo esc_html( $county_libraries[0]['county'] ) ?></th></tr>
+    </thead>
+    <tbody>
     <?php foreach( $county_libraries as $library ) : ?>
-      <li>
-        <a href="<?php echo esc_url( $library['url'] ) ?>"><?php echo esc_html( $library['name'] ) ?></a>:
-        <?php printf( esc_html(
-          _n( '%d eksemplar', '%d eksemplarer', $library['copies'], 'lsb'  ) ), $library['copies'] );
-        ?>
-        <a href="<?php echo esc_url( $library['book_url'] ) ?>"><?php esc_html_e('(sjekk status)', 'lsb'); ?></a>
-      </li>
+      <tr>
+        <td>
+          <a href="<?php echo esc_url( $library['url'] ) ?>">
+            <?php echo esc_html( $library['name'] ) ?>
+          </a>
+        </td>
+        <td>
+          <a class="btn btn-default" target="_blank" href="<?php echo esc_url( $library['book_url'] ) ?>">
+            <?php _e('Lån boka', 'lsb'); ?>
+          </a>
+        </td>
+      </tr>
     <?php endforeach; ?>
-    </ul>
+    </tbody>
+  </table>
   <?php endforeach; ?>
 </div>
 <?php endif; ?>
