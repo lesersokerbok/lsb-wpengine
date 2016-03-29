b39446d169d02b9657917cab065dafbc1342c398
Benedicte Raae <raae@bgraphic.no>
2016-03-29 17:33:41 +0200

    Using jquery to only show one county at the time.

--- wp-content/themes/lsb-boksok.no-theme/templates/book-partials/library-status.php
+++ wp-content/themes/lsb-boksok.no-theme/templates/book-partials/library-status.php
@@ -6,13 +6,27 @@ $counties = get_post_meta($post->ID, 'lsb_library_status', true);
 
 <?php if( $counties ) : ?>
 <div class="content-part library-status">
-
-  <?php foreach( $counties as $county_libraries ) : ?>
   <table class="table">
     <thead>
-      <tr><th colspan="2"><?php echo esc_html( $county_libraries[0]['county'] ) ?></th></tr>
+      <tr>
+        <th colspan="2">
+          <form class="form-inline">
+           <label for="countySelect"><?php _e( 'Lån boka på ditt bibliotek:', 'lsb' ); ?></label>
+            <select class="form-control" id="countySelect">
+              <option><?php _e('Velg fylke', 'lsb') ?></option>
+              <?php foreach( $counties as $key => $county_libraries ) : ?>
+                <?php if( $key ) : ?>
+                <option value="<?php echo sanitize_title($key) ?>"><?php echo esc_html( $key ); ?></option>
+                <?php endif; ?>
+              <?php endforeach; ?>
+            </select>
+          </form>
+        </th>
+      </tr>
     </thead>
-    <tbody>
+
+  <?php foreach( $counties as $key => $county_libraries ) : ?>
+    <tbody class="hidden county <?php echo sanitize_title( $key ); ?>">
     <?php foreach( $county_libraries as $library ) : ?>
       <tr>
         <td>
@@ -28,7 +42,7 @@ $counties = get_post_meta($post->ID, 'lsb_library_status', true);
       </tr>
     <?php endforeach; ?>
     </tbody>
-  </table>
   <?php endforeach; ?>
+  </table>
 </div>
 <?php endif; ?>
