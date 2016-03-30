9030598af048c1c5810516ed8506f67904670472
Benedicte Raae <raae@bgraphic.no>
2016-03-30 15:53:47 +0200

    Remove look-inside styling and also unused term-icon styling

--- wp-content/themes/lsb-boksok.no-theme/templates/book-partials/cover.php
+++ wp-content/themes/lsb-boksok.no-theme/templates/book-partials/cover.php
@@ -1,11 +1,13 @@
 <?php if (has_post_thumbnail()): ?>
 <div class="cover content-part">
+  <div class="thumbnail">
   <?php if ( get_field('lsb_look_inside')): ?>
-    <a class="thumbnail look-inside" href="<?php the_field('lsb_look_inside'); ?>" target="_blank">
+    <a href="<?php the_field('lsb_look_inside'); ?>" target="_blank">
       <?php the_post_thumbnail('large', array('class' => 'look-inside')); ?>
     </a>
   <?php else : ?>
-    <div class="thumbnail"><?php the_post_thumbnail('large'); ?></div>
+    <?php the_post_thumbnail('large'); ?>
   <?php endif; ?>
+  </div>
 </div>
 <?php endif; ?>
