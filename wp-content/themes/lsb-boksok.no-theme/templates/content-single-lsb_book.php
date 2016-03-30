d46107e2ddb4a2de25a9f545627811d3354dbc35
Benedicte Raae <raae@bgraphic.no>
2016-03-30 14:32:06 +0200

    Removed embeds from current location. Library status after cover even on small devices.

--- wp-content/themes/lsb-boksok.no-theme/templates/content-single-lsb_book.php
+++ wp-content/themes/lsb-boksok.no-theme/templates/content-single-lsb_book.php
@@ -10,21 +10,17 @@
   <div class="aside hidden-sm hidden-xs">
     <?php get_template_part('templates/book-partials/cover'); ?>
     <?php get_template_part('templates/book-partials/library-status'); ?>
-    <?php get_template_part('templates/book-partials/embeds'); ?>
   </div>
 
   <div class="entry-content">
     <?php get_template_part('templates/book-partials/meta1'); ?>
     <div class="visible-sm visible-xs">
       <?php get_template_part('templates/book-partials/cover'); ?>
+      <?php get_template_part('templates/book-partials/library-status'); ?>
     </div>
     <?php get_template_part('templates/book-partials/review'); ?>
     <?php get_template_part('templates/book-partials/quote'); ?>
     <?php get_template_part('templates/book-partials/more'); ?>
-    <div class="visible-sm visible-xs">
-      <?php get_template_part('templates/book-partials/library-status'); ?>
-      <?php get_template_part('templates/book-partials/embeds'); ?>
-    </div>
     <?php get_template_part('templates/book-partials/icon-nav'); ?>
     <?php get_template_part('templates/book-partials/meta2'); ?>
     <?php get_template_part('templates/book-partials/lsb-supported'); ?>
