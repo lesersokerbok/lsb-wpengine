d5f8e7164e4b80d2e4b941b00b621c81f64eb864
Benedicte Raae <raae@bgraphic.no>
2016-04-19 08:26:54 +0200

    Moved page sections into base theme.

--- wp-content/themes/lsb-base-theme/template-frontpage.php
+++ wp-content/themes/lsb-base-theme/template-frontpage.php
@@ -6,10 +6,10 @@ Template Name: Frontpage Template
 
 <?php if ( have_rows('sections') ): ?>
   <?php while ( have_rows('sections') ) : the_row(); ?>
-    <div class="frontpage-section frontpage-section-<?php echo get_row_layout(); ?>">
-      
+    <div class="page-section page-section-<?php echo get_row_layout(); ?>">
+
       <?php get_template_part('templates/section', get_row_layout()); ?>
-      
+
     </div>
   <?php endwhile; ?>
 <?php endif; ?>
