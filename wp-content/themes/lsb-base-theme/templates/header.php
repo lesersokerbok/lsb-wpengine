0258befe357469268aedcd1319265957e5784a98
Benedicte Raae <raae@bgraphic.no>
2016-05-18 14:26:06 +0200

    Better nav styling on small displays

--- wp-content/themes/lsb-base-theme/templates/header.php
+++ wp-content/themes/lsb-base-theme/templates/header.php
@@ -5,15 +5,19 @@
       <ul class="nav navbar-nav pull-left">
         <li>
           <a class="navbar-nav-brand" href="<?php echo home_url(); ?>/">
-            <?php bloginfo( 'name' ) ?>
 						<?php get_template_part('templates/logo'); ?>
+            <?php bloginfo( 'name' ) ?>
           </a>
         </li>
       </ul>
-      
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
-        <?php esc_html_e( 'Meny', 'lsb' ); ?>
-      </button>
+
+			<ul class="toggle nav navbar-nav pull-right">
+				<li>
+					<a href="#" class="collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+						<?php esc_html_e( 'Meny', 'lsb' ); ?>
+					</a>
+				</li>
+			</ul>
     </div>
 
     <nav class="collapse navbar-collapse" role="navigation">
