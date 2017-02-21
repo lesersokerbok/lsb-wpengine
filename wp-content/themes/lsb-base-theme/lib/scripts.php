3baba830508748baca888836775594504117d211
Benedicte Raae <benedicte@lillylabs.no>
2017-02-21 10:16:41 +0100

    Npmscripts (#186)
    * Updated package json name and urls, remove version
    * Moved from grunt to npm scripts

--- wp-content/themes/lsb-base-theme/lib/scripts.php
+++ wp-content/themes/lsb-base-theme/lib/scripts.php
@@ -22,63 +22,25 @@ function roots_scripts() {
    * Read the asset names from assets-manifest.json
    */
 
+	// Development assets
 	$assets = array(
-      'css'       => '/assets/css/main.css',
-      'js'        => '/assets/js/scripts.js',
-      'modernizr' => '/assets/vendor/modernizr/modernizr.js',
-      'jquery'    => '//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.js'
+      'css'       => '/assets/css/bundle.temp.css',
+      'js'        => '/assets/js/bundle.temp.js',
+      //'modernizr' => '/assets/vendor/modernizr/modernizr.js'
     );
 
-  if (WP_ENV !== 'development') {
-
-		$manifest_json = file_get_contents(get_template_directory() . '/assets/manifest.json');
-    $manifest     = json_decode($manifest_json, true);
-
-		$assets['css'] = '/'.$manifest['assets/css/main.min.css'];
-		$assets['js'] = '/'.$manifest['assets/js/scripts.min.js'];
-
+  if ('development' !== WP_ENV) {
+		// Production assets
+		$assets['css'] = '/assets/css/bundle.min.css';
+		$assets['js'] = '/assets/js/bundle.min.js';
   }
 
-	wp_enqueue_style('roots_css', get_template_directory_uri() . $assets['css'], false, null);
+	wp_enqueue_style('lsb_css', get_template_directory_uri() . $assets['css'], false, null);
 
-
-  /**
-   * jQuery is loaded using the same method from HTML5 Boilerplate:
-   * Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
-   * It's kept in the header instead of footer to avoid conflicts with plugins.
-   */
-  if (!is_admin() && current_theme_supports('jquery-cdn')) {
-    wp_deregister_script('jquery');
-    wp_register_script('jquery', $assets['jquery'], array(), null, false);
-    add_filter('script_loader_src', 'roots_jquery_local_fallback', 10, 2);
-  }
-
-  if (is_single() && comments_open() && get_option('thread_comments')) {
-    wp_enqueue_script('comment-reply');
-  }
-
-	wp_enqueue_script('modernizr', get_template_directory_uri() . $assets['modernizr'], array(), null, false);
-  wp_enqueue_script('jquery');
-	wp_enqueue_script('roots_js', get_template_directory_uri() . $assets['js'], array(), null, true);
+	//wp_enqueue_script('modernizr', get_template_directory_uri() . $assets['modernizr'], array(), null, false);
+	wp_enqueue_script('lsb_js', get_template_directory_uri() . $assets['js'], array( 'jquery' ), null, true);
 }
 add_action('wp_enqueue_scripts', 'roots_scripts', 100);
-
-// http://wordpress.stackexchange.com/a/12450
-function roots_jquery_local_fallback($src, $handle = null) {
-  static $add_jquery_fallback = false;
-
-  if ($add_jquery_fallback) {
-    echo '<script>window.jQuery || document.write(\'<script src="' . get_template_directory_uri() . '/assets/vendor/jquery/dist/jquery.min.js?1.11.1"><\/script>\')</script>' . "\n";
-    $add_jquery_fallback = false;
-  }
-
-  if ($handle === 'jquery') {
-    $add_jquery_fallback = true;
-  }
-
-  return $src;
-}
-add_action('wp_head', 'roots_jquery_local_fallback');
 
 /**
  * Google Analytics snippet from HTML5 Boilerplate
