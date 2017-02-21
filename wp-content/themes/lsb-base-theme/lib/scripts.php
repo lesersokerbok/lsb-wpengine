1dad0a077bda0ccfbee8e70cbbfe5098b97e091a
Benedicte Raae <benedicte@lillylabs.no>
2017-02-21 12:53:44 +0100

    Busted (#187)
    * Cache busting by using manifest.json file
    * Removed reference to modenizer and jquery scripts
    * Fixed build:cacheBust missing from packackage

--- wp-content/themes/lsb-base-theme/lib/scripts.php
+++ wp-content/themes/lsb-base-theme/lib/scripts.php
@@ -2,15 +2,6 @@
 /**
  * Scripts and stylesheets
  *
- * Enqueue stylesheets in the following order:
- * 1. /theme/assets/css/main.css?5c998229dee9462e
- *
- * Enqueue scripts in the following order:
- * 1. jquery-1.11.1.min.js via Google CDN
- * 2. /theme/assets/js/vendor/modernizr.min.js
- * 3. /theme/assets/js/scripts.js (in footer)
- * 4. /child-theme/style.css (if a child theme is activated)
- *
  * Google Analytics is loaded after enqueued scripts if:
  * - An ID has been defined in config.php
  * - You're not logged in as an administrator
@@ -24,21 +15,34 @@ function roots_scripts() {
 
 	// Development assets
 	$assets = array(
-      'css'       => '/assets/css/bundle.temp.css',
-      'js'        => '/assets/js/bundle.temp.js',
-      //'modernizr' => '/assets/vendor/modernizr/modernizr.js'
+  	'css'       => 'assets/css/bundle.temp.css',
+    'js'        => 'assets/js/bundle.temp.js'
     );
+
+	$version = array(
+		'css'				=> time(),
+		'js'				=> time()
+	);
 
   if ('development' !== WP_ENV) {
 		// Production assets
-		$assets['css'] = '/assets/css/bundle.min.css';
-		$assets['js'] = '/assets/js/bundle.min.js';
+		$assets['css'] = 'assets/css/bundle.min.css';
+		$assets['js'] = 'assets/js/bundle.min.js';
+
+		$manifest_json = file_get_contents(get_template_directory() . '/assets/manifest.json');
+  	$manifest     = json_decode($manifest_json, true);
+
+		$version['css'] = $manifest[$assets['css']];
+		$version['js'] = $manifest[$assets['js']];
   }
 
-	wp_enqueue_style('lsb_css', get_template_directory_uri() . $assets['css'], false, null);
+	$assets['css'] = get_template_directory_uri() . '/' . $assets['css'];
+	$assets['js'] = get_template_directory_uri(). '/' . $assets['js'];
+
+	wp_enqueue_style('lsb_css', $assets['css'], array(), $version['css'], 'all');
 
 	//wp_enqueue_script('modernizr', get_template_directory_uri() . $assets['modernizr'], array(), null, false);
-	wp_enqueue_script('lsb_js', get_template_directory_uri() . $assets['js'], array( 'jquery' ), null, true);
+	wp_enqueue_script('lsb_js', $assets['js'], array( 'jquery' ), $version['js'], true);
 }
 add_action('wp_enqueue_scripts', 'roots_scripts', 100);
 
