0a7ba73c011aabb6449da74b7933bde9639de298
Benedicte Raae <raae@bgraphic.no>
2017-04-10 12:52:42 +0200

    Add isset to remove debug warnings

--- wp-content/themes/lsb-base-theme/lib/lsb-post.php
+++ wp-content/themes/lsb-base-theme/lib/lsb-post.php
@@ -4,7 +4,7 @@ class LSB_Post extends TimberPost {
 
 	public function lsb_read_more() {
 		$post_type_obj = get_post_type_object( $this->post_type );
-		if($post_type_obj->labels->lsb_read_more) {
+		if(isset($post_type_obj->labels->lsb_read_more)) {
 			return sprintf($post_type_obj->labels->lsb_read_more, $this->post_title );
 		} else {
 			return __('Les hele artikkelen', 'lsb');
