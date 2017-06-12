605b0d6ef1d7eb20dbd8fffab4a61b32745e9cef
Benedicte Raae <raae@bgraphic.no>
2017-06-12 12:20:03 +0200

    Cleaned up voice tracking and added scroll tracking #251

--- wp-content/themes/lsb-base-theme/assets/js/_ga.js
+++ wp-content/themes/lsb-base-theme/assets/js/_ga.js
@@ -1,5 +1,20 @@
 (function ($) {
 
+  function debounce(func, wait, immediate) {
+    var timeout;
+    return function () {
+      var context = this, args = arguments;
+      var later = function () {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  };
+
   if (typeof (ga) !== typeof (Function)) {
     return;
   }
@@ -13,20 +28,31 @@
 
   $('.lsb_voice__stop').each(function () {
     $(this).bind('click', function (e) {
-      ga('stop', 'event', 'Voice', 'stop');
+      ga('send', 'event', 'Voice', 'stop');
     });
   });
 
   $('.lsb_voice__settings').each(function () {
     $(this).bind('click', function (e) {
-      ga('stop', 'event', 'Voice', 'settings');
+      ga('send', 'event', 'Voice', 'settings');
     });
   });
 
   $('.lsb_voice__help').each(function () {
     $(this).bind('click', function (e) {
-      ga('stop', 'event', 'Voice', 'help');
+      ga('send', 'event', 'Voice', 'help');
     });
   });
+
+  // Track section scroll events
+  var scroll = debounce(function () {
+    ga('send', 'event', 'Scroll', 'scroll');
+  }, 250, true);
+
+  $('.lsb_section__scroll').click(function (e) {
+    ga('send', 'event', 'Scroll', 'click');
+  });
+
+  $('.lsb_section__body').scroll(scroll);
 
 })(jQuery);
