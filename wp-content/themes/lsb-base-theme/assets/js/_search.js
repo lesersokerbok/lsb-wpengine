d9396ba73bad8b95356c8cf29bc2df6afcccb2a4
Benedicte Raae <raae@bgraphic.no>
2017-06-12 08:48:18 +0200

    Beautify

--- wp-content/themes/lsb-base-theme/assets/js/_search.js
+++ wp-content/themes/lsb-base-theme/assets/js/_search.js
@@ -1,5 +1,5 @@
 
-(function($) {
+(function ($) {
 
 	function addRelevantMetaAndContent(book) {
 		var useRelevantContent = true;
@@ -10,36 +10,36 @@
 		relevant_meta.partof = [];
 		relevant_meta.audience = [];
 
-		for ( var tax_key in book._highlightResult.taxonomies ) {
+		for (var tax_key in book._highlightResult.taxonomies) {
 			var tax_terms = book._highlightResult.taxonomies[tax_key];
-			for ( var term_index in tax_terms ) {
+			for (var term_index in tax_terms) {
 				var tax_term = tax_terms[term_index];
-				if ( tax_term.matchLevel !== 'none' || tax_key === 'lsb_tax_author') {
-					if( tax_key === 'lsb_tax_author' || tax_key === 'lsb_tax_illustrator' || tax_key === 'lsb_tax_translator') {
-						relevant_meta.creators.push({value: tax_term.value, permalink: book.taxonomies_permalinks[tax_key][term_index]});
-					} else if( tax_key === 'lsb_tax_topic') {
-						relevant_meta.topics.push({value: tax_term.value, permalink: book.taxonomies_permalinks[tax_key][term_index]});
-					} else if( tax_key === 'lsb_tax_series' || tax_key === 'lsb_tax_list' ) {
-						relevant_meta.partof.push({value: tax_term.value, permalink: book.taxonomies_permalinks[tax_key][term_index]});
-					} else if( tax_key === 'lsb_tax_age' || tax_key === 'lsb_tax_audience' ) {
-						relevant_meta.audience.push({value: tax_term.value, permalink: book.taxonomies_permalinks[tax_key][term_index]});
+				if (tax_term.matchLevel !== 'none' || tax_key === 'lsb_tax_author') {
+					if (tax_key === 'lsb_tax_author' || tax_key === 'lsb_tax_illustrator' || tax_key === 'lsb_tax_translator') {
+						relevant_meta.creators.push({ value: tax_term.value, permalink: book.taxonomies_permalinks[tax_key][term_index] });
+					} else if (tax_key === 'lsb_tax_topic') {
+						relevant_meta.topics.push({ value: tax_term.value, permalink: book.taxonomies_permalinks[tax_key][term_index] });
+					} else if (tax_key === 'lsb_tax_series' || tax_key === 'lsb_tax_list') {
+						relevant_meta.partof.push({ value: tax_term.value, permalink: book.taxonomies_permalinks[tax_key][term_index] });
+					} else if (tax_key === 'lsb_tax_age' || tax_key === 'lsb_tax_audience') {
+						relevant_meta.audience.push({ value: tax_term.value, permalink: book.taxonomies_permalinks[tax_key][term_index] });
 					}
 				}
 
-				if ( tax_term.matchLevel !== 'none' ) {
+				if (tax_term.matchLevel !== 'none') {
 					useRelevantContent = false;
 				}
 			}
 		}
 
-		if( book._highlightResult.post_title.matchLevel !== 'none') {
+		if (book._highlightResult.post_title.matchLevel !== 'none') {
 			useRelevantContent = false;
 		}
 
-		if(useRelevantContent) {
-			for ( var snippet_index in book._snippetResult ) {
+		if (useRelevantContent) {
+			for (var snippet_index in book._snippetResult) {
 				var snippet = book._snippetResult[snippet_index];
-				if( snippet.matchLevel !== 'none') {
+				if (snippet.matchLevel !== 'none') {
 					book.relevant_content = snippet.value;
 					break;
 				}
@@ -67,7 +67,7 @@
 				'lsb_quote:20'
 			],
 		},
-		searchFunction: function(helper) {
+		searchFunction: function (helper) {
 			console.log("Søk", search.helper.state.query)
 			var savedPage = helper.state.page;
 			var isSearchPage = $('body').hasClass('search');
@@ -75,7 +75,7 @@
 			var searchResults = $('#search-page');
 			var pageNav = $('.lsb-navbar-page');
 
-			if ( search.helper.state.query === '' && !isSearchPage ) {
+			if (search.helper.state.query === '' && !isSearchPage) {
 				mainSections.show();
 				pageNav.show();
 				searchResults.hide();
@@ -94,7 +94,7 @@
 	});
 
 	// Search box widget
-	$('#algolia-form input').each(function() {
+	$('#algolia-form input').each(function () {
 		search.addWidget(
 			instantsearch.widgets.searchBox({
 				container: this,
@@ -111,7 +111,7 @@
 			container: '#algolia-hits',
 			hitsPerPage: 30,
 			transformData: {
-				item: function(book) {
+				item: function (book) {
 					addRelevantMetaAndContent(book);
 					return book;
 				},
@@ -140,8 +140,8 @@
 	// Start
 	search.start();
 
-	$searchInput = $('#algolia-form').each(function() {
-		$(this).bind('submit', function(e) {
+	$searchInput = $('#algolia-form').each(function () {
+		$(this).bind('submit', function (e) {
 			e.preventDefault();
 			$(this).find('input').blur();
 			$(this).find('button').blur();
