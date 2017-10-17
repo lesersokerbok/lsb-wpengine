1deecd64e50c89bc7f0fe798a9d33c90d84a7afb
Benedicte Raae <raae@bgraphic.no>
2017-10-17 10:01:19 +0200

    Removed unused utils.

--- wp-content/plugins/lsb-boksok-core/lsb-boksok-core.php
+++ wp-content/plugins/lsb-boksok-core/lsb-boksok-core.php
@@ -10,9 +10,6 @@
 namespace LSB\Boksok\Core;
 
 include('class-lsb-book.php');
-include('lsb-filter-query-util.php');
-include('lsb-page-sections-util.php');
-include('taxonomy-util.php');
 
 new LsbBook();
 
@@ -23,18 +20,3 @@ function boksok_get_search_form($form) {
 	return $form;
 }
 add_filter('get_search_form', __NAMESPACE__ .'\\boksok_get_search_form');
-
-add_filter( 'query_vars', function ($query_vars) {
-  return array_merge($query_vars, \LsbFilterQueryUtil::possible_query_vars_for_lsb_book());
-});
-
-function add_book_page_filter_css( $classes) {
-  if(is_page_template( 'template-boksok-book-page.php' )) {
-    $term_objects = get_field('lsb_book_page_filter_lsb_tax_lsb_cat');
-    $term_slugs = TaxonomyUtil::get_terms_slug_array($term_objects);
-    return array_merge($classes, $term_slugs);
-  }
-
-  return $classes;
-}
-add_filter( 'body_class', __NAMESPACE__ .'\\add_book_page_filter_css', 100, 2);
